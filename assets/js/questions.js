000351b6fda3fd1dedaa3269e92a81d88e6e14b7
GrahamWilliamson234 <graham@dazl.ie>
2022-12-17 21:32:18 +0000

    js questions array added

---
+++ assets/js/questions.js
@@ -0,0 +1,88 @@
+// Array of Questions
+const quiz = [
+    {
+        q:'Where would be if you were standing on the Spanish Steps?',
+        options:['Madrid','Rome','Miami'],
+        answer:1
+    },
+    {
+        q:'What country would you find Mount Kilimanjaro?',
+        options:['USA','Tanzania','South Africa'],
+        answer:1
+    },
+    {
+        q:'Who was the 25th President of the USA?',
+        options:['Grover Cleveland','William McKinley','Theodore Roosevelt'],
+        answer:1
+    },
+    {
+        q:'Which planet is closest to the Sun?',
+        options:['Mars','Earth','Mercury'],
+        answer:2
+    },
+    {
+        q:'What did Snickers bars used to be called before the name was changed in 1990?',
+        options:['Sprint','Track','Marathon'],
+        answer:2
+    },
+    {
+        q:'In what year was the first email sent?',
+        options:['1969','1971','1973'],
+        answer:1
+    },
+    {
+        q:'Who was the 1st President of the Ireland?',
+        options:['Douglas Hyde','Éamon de Valera','W.T Cosgrave'],
+        answer:0
+    },
+    {
+        q:'263 + 65 = 318 ?',
+        options:['true','false'],
+        answer:1
+    },
+    {
+        q:'What company was originally called Cadabra?',
+        options:['Google','Instagram','Amazon'],
+        answer:2
+    },
+    {
+        q:'What is 32 x 7?',
+        options:['199','156','224'],
+        answer:2
+    },
+    {
+      q:'Windows, macOS, and Linux are examples of ________.?',
+      options:['Web browsers','Mobile devices','Operating systems'],
+      answer:2,
+    },
+    {
+      q:'What does "GUI" stand for?',
+      options:['Global user index','Graphical user interface','golu use iphone'],
+      answer:1,
+    },
+    {
+      q:'Mark Zuckerberg is the owner of  ?',
+      options:['Facebook','Google','Linux'],
+      answer:0,
+    },
+    {
+      q:'What company was initally known as Blue Ribbon Sports?',
+      options:['Nike','Puma','Adidas'],
+      answer:0,
+    },
+    {
+      q:'Name Hermoine Grangers cat in the Harry Potter movies/books?',
+      options:['Crookshanks','Tiger','Ginger'],
+      answer:0,
+    },
+    {
+      q:'Who is Saul Hudson?',
+      options:['Prince','Axl Rose','Slash'],
+      answer:2,
+    },
+    {
+      q:'In what year did Ireland become a Republic?',
+      options:['1916','1922','1949'],
+      answer:2,
+    }
+   ];  
