f5b3b3ddddafa8ad6edaf1c4f3fc6c8f471e8663
GrahamWilliamson234 <graham@dazl.ie>
2022-12-17 21:27:20 +0000

    js set question, options added

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,4 +21,53 @@ const questionNumber = document.querySelector(".question-number");
     }
  }
 
+ // set question number, question and options
+ function getNewQuestion(){
+    // set question number 
+    questionNumber.innerHTML = "Question " + (questionCounter + 1) + " of " + questionLimit;
+
+    // set question text
+    const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.q;
+    // get the position of 'questionIndex' from the availableQuestion Array
+    const index1= availableQuestions.indexOf(questionIndex);
+    // remove the 'questionIndex' from the availableQuestion Array, so that the question does not repeat
+    availableQuestions.splice(index1,1);
+    if(currentQuestion.hasOwnProperty("img")){
+       const img = document.createElement("img");
+       img.src = currentQuestion.img;
+       questionText.appendChild(img);
+    }
+
+    // set options
+    const optionLen = currentQuestion.options.length;
+    // push options into availableOptions Array
+    for(let i=0; i<optionLen; i++){
+       availableOptions.push(i);
+    }
+    optionContainer.innerHTML = '';
+    let animationDelay = 0.10;
+    // create options in html
+    for(let i=0; i<optionLen; i++){
+       // random option
+       const optonIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
+       // get the position of 'optonIndex' from the availableOptions Array
+       const index2 =  availableOptions.indexOf(optonIndex);
+       // remove the  'optonIndex' from the availableOptions Array , so that the option does not repeat
+       availableOptions.splice(index2,1);
+       const option = document.createElement("div");
+       option.innerHTML = currentQuestion.options[optonIndex];
+       option.id = optonIndex;
+       option.style.animationDelay =animationDelay + 's';
+       animationDelay = animationDelay + 0.20;
+       option.className = "option";
+       optionContainer.appendChild(option);
+       option.setAttribute("onclick","getResult(this)");
+    }
+   console.log(availableQuestions);
+   console.log(availableOptions);
+    questionCounter++;
+ }
+
  
